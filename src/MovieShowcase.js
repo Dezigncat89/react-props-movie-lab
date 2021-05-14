63d1e3507759e704b2eed27b3da9bb1bb08a3522
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-13 17:05:01 -0700

    Update MovieShowcase.js

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -8,16 +8,54 @@ export default class MovieShowcase extends Component {
 
     const movieData = [];
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 9; i++) {
         movieData.push({
-            ,
-            country: chance.country({ full: true })
+          Title: chance.title(),
+          Rating: chance.IMDBRating(),
+          Genres: chance.genres(),
+          
+            
+            
         });
     }
 
-    this.state = { people };
+    this.state = { movieData };
 }
 
+  render() {
+    return (
+      <div className="CardBack">
+    {this.state.movieData.map((movie, index) => (
+        <h2 mTitle={this.props.titleSrc} alt={this.props.title} />
+        <h2 movie.title}</h2>
+         <p IMDBRating1={this.props.Rating} alt={this.props.ratingList} />
+        <p movie.IMDBRating}</p>
+         <p listGenres={this.props.genres} alt={this.props.allGenres} />
+        <p movie.genres}</p>
+    ))}
+    </div>);
+}
+
+ render() {
+    return (
+      <div className="CardFront">
+    {this.state.movieData.map((movie, index) => (
+        <img src={this.props.posterSrc} alt={this.props.poster} />
+        <h2>{movie.poster}</h2>
+       
+    ))}
+    </div>);
+}
+
+CardFront.defaultProps = {
+  posterSrc: 'https://i2.wp.com/www.filmywapzone.com/wp-content/uploads/2021/04/Best-Hollywood-Movies-of-All-Times.jpg'
+}
+cardBack.defaultProps = {
+  titleSrc: "Unknown",
+  Rating: null,
+  genres: 'No Genres Found'
+}
+  
   render() {
     return (
       <div id="movie-showcase">
